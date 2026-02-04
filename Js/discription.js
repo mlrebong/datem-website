d5e9655ffe03b183fbb9bfb01997bd9c352d4f2a
LLENO <mlrlleno@datem.com.ph>
2026-02-04 16:01:31 +0800

    Clean repository (remove large files and history)

---
+++ Js/discription.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:38a2a4c276b752d76b035862c1209b23ef8f3a155b09c4bd35f409f4e180b8d7
+size 5372
---
+++ Js/script.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6daa09debbc06cc8cd3132a34a2419a9031a11d0d0e92428dc2754fd6e63d3af
+size 1856
